e606583d78c59b2ebf20776b7085a11ace350005
Prince Kumar Singh <38320278+iprincekumarsingh@users.noreply.github.com>
2022-03-04 09:32:37 +0530

    ascSort.c

---
+++ ascSort.c
@@ -0,0 +1,18 @@
+// Sort Array Using Bubble Sort in C or any language
+
+#include <stdio.h>
+int ascSort(int arr[10])
+{
+    for (int i = 0; i < 10 - 1; i++)
+    {
+        for (int j = 0; j < 10 - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+#to use this file
+include "filename.h"
+
+#to call the function in your main function
+
+ascSort(arrayname);
